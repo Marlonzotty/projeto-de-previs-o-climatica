1920d947edba6c5a3665e95aaca02e45ba986bf8
Marlonzotty <112110890+Marlonzotty@users.noreply.github.com>
2023-01-05 17:46:07 -0300

    commit atualizado

--- index.html/js/scripts.js
+++ index.html/js/scripts.js
@@ -8,14 +8,21 @@ const apiCountryURL = "https://countryflagsapi.com/png/";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
-
+//cidade que aparece na tela 
 const cityElement = document.querySelector("#city");
-const tempElement = document.querySelector("Temperature span");
-const descElement = document.querySelector("Description");
-const weatherIconElement = document.querySelector("#wheather-icon");
+//temperatura que aparece na tela
+const tempElement = document.querySelector("#temperature span");
+//descrição do clima que aparece na tela 
+const descElement = document.querySelector("#description");
+//API do icone que  aparece na tela 
+const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity span");
+// Humidade do local 
+const humidityElement = document.querySelector("#humidity span");
+// ventos do local
 const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
 
 //função
 
@@ -29,19 +36,40 @@ const getWeatherData = async (city) => {
 
 const showWeatherData = async (city) => {
     const data = await getWeatherData(city);
-    console.log(data)
+
     cityElement.innerText = data.name;
-    tempElement.innerText = data.weather[0].description;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    //countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+
+
+    weatherContainer.classList.remove("hide");
+
+
+
+
 };
 
 //eventos
+//
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
     const city = cityInput.value;
     showWeatherData(city);
+
+});
+// aqui é colocado o evento  de que quando você apert enter ja é diretamente pesquisada a cidade
+cityInput.addEventListener("keyup", (e) => {
+    if (e.code === "Enter") {
+        const city = e.target.value;
+        showWeatherData(city);
+    }
 });
 
-
+// unsplash
 
 
 
